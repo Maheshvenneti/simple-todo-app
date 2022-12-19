9ef949c68b005fc2187e88bbc0a6c5ddfc0f530b
mahesh <maheshvenneti123@gmail.com>
2022-12-19 14:42:56 +0530

    git init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  
+  const [input, setInput] = useState([]);
+
+  const [todos, setTodos] = useState([]);
+  const [toggle, setToggle] = useState(true);
+  const [isEditItem, setIsEditItem] = useState(null);
+
+  const ChangeHandler = (e) => {
+    setInput(e.target.value);
+  };
+
+  const SubmitHandler =(e)=>{
+    e.preventDefault();
+    if(!input){
+      window.alert("Please enter a valid input");
+    } else if(input && !toggle){
+      setTodos(
+        todos.map((item)=>{
+          if(item.id===isEditItem){
+            return{...item, name:input}
+          }
+          return item;
+        })
+      )
+      setInput('')
+      setToggle(true);
+
+    }else{
+      let newTodos = {id: Math.floor(Math.random() * 100000), name: input}
+      setTodos([...todos, newTodos]);
+      setInput('');
+    }
+  }
+
+
+  const DeleteHandler = (index) =>{
+    let newtodo = todos.filter((item)=>index!== item.id)
+    setTodos(newtodo)
+  }
+
+  const editItem =(id) =>{
+    let newEditItem = todos.find((item)=>item.id===id)
+    setInput(newEditItem.name)
+    setToggle(false)
+    setIsEditItem(newEditItem.id);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <form onSubmit={SubmitHandler}>
+        <input type="text" name="input" value={input} onChange={ChangeHandler} />{" "}
+        &nbsp;
+        {toggle ? (
+          <input value="add" name="add" type="submit" />
+        ) : (
+          <input value="edit" name="edit" type="submit" />
+        )}
+      </form>
+      <br />
+
+      {todos.map((item) => {
+        return (
+          <div key={item.id}>
+            {item.name} &nbsp;{" "}
+            <button onClick={() => DeleteHandler(item.id)}>delete</button>&nbsp;
+            <button onClick={() => editItem(item.id)}>edit</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
